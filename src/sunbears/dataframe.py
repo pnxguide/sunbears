4c105a0f0f8dec777a42729088141152018e7c69
pnxguide <yuttapichai.ke@gmail.com>
2024-12-04 14:39:53 +0700

    updated parser

--- src/sunbears/dataframe.py
+++ src/sunbears/dataframe.py
@@ -29,9 +29,7 @@ class Filter:
         return f"{self.lvalue} {self.op} {self.rvalue}"
 
 class DataFrame:
-    # TODO: 
     def __init__(self, column_names: list[str], column_types: list[type], rows: list[object]):
-        print("Initializing a new DataFrame")
         self.column_names = column_names
         self.column_types = column_types
         self.rows = rows
@@ -75,6 +73,22 @@ class DataFrame:
         print("Find the minimum value among all the values")
         return 0
 
-    # TODO:
     def __str__(self):
-        return "Unimplemented DataFrame"
+        dataframe_string = ""
+
+        # Print column names
+        for i in range(len(self.column_names)):
+            dataframe_string += f"{self.column_names[i]}:{str(self.column_types[i])}"
+            if i < len(self.column_names) - 1:
+                dataframe_string += "|"
+        dataframe_string += "\n"
+
+        # Print data
+        for row in self.rows:
+            for i in range(len(row)):
+                dataframe_string += f"{row[i]}"
+                if i < len(row) - 1:
+                    dataframe_string += "|"
+            dataframe_string += "\n"
+        
+        return dataframe_string
--- src/sunbears/parser.py
+++ src/sunbears/parser.py
@@ -1,46 +1,48 @@
 from sunbears.dataframe import DataFrame
 
-def interpret(val):
+def infer_type(val):
     try:
-        return int(val)
+        int(val)
+        return int
     except ValueError:
         try:
-            return float(val)
+            float(val)
+            return float
         except ValueError:
-            return val
+            return str
 
 class Parser:
-    # TODO:
     def read_csv(self, filepath: str) -> DataFrame:
         f = open(filepath, "r")
         content = f.readlines()
-        print(content)
 
-        # TODO: Get the column names as a list of str
+        # Get the column names as a list of str
         header_line = content[0].strip()
         column_names = header_line.split(",")
 
-        # TODO: Infer each column type
+        # Infer each column type and append data into rows
         i = 1
         n = len(content)
         column_types = [int, int, int]
-        while i < n:
+        rows = []
+        for i in range(1, n):
             stripped_line = content[i].strip()
             tokens = stripped_line.split(",")
 
-            for j in range(len(tokens)):
-                inferred_token = interpret(tokens[j])
-                inferred_type = type(inferred_token)
+            tuple_list = []
 
+            for j in range(len(tokens)):
+                inferred_type = infer_type(tokens[j])
+                inferred_token = inferred_type(tokens[j])
+                
                 if inferred_type == str:
                     column_types[j] = str
                 elif inferred_type == float and column_types[j] != str:
                     column_types[j] = float
 
+                tuple_list.append(inferred_token)
+            
+            rows.append(tuple(tuple_list))
             i = i + 1
 
-        print(column_types)
-
-        # TODO: Get all the rows as a list of tuples
-
-        return DataFrame(column_names, column_types, [])
+        return DataFrame(column_names, column_types, rows)
